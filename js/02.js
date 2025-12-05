1ec91f1b57932ec2cb056553df50a9c162597719
vetdy03 <cocaever0@gmail.com>
2025-12-05 00:21:50 -0400

    javascript - 105. Variables con let y const

---
+++ js/02.js
@@ -0,0 +1,17 @@
+//variables con let
+let producto = "audifonos gamer"; //iniciar variable y asignar valor
+let disponible; //iniciar variable sin valor
+
+// producto = true; //reasignando el valor de la variable
+
+let pro1 = "computadora",
+  pro2 = "tablet",
+  pro3 = "celular";
+
+//estilos para las variables
+let nombre_producto = "monitor HD"; //underscore
+let nombreProducto = "monitor HD"; //camelcase
+let NombreProducto = "monitor HD"; //Pascalcase
+let nombreproducto = "monitor HD"; //lowercase
+console.log(producto);
+/*var nombre = "Vetdy";*/
---
+++ js/03.js
@@ -0,0 +1,17 @@
+//variables con let
+const producto = "audifonos gamer"; //iniciar variable y asignar valor
+const disponible; //iniciar variable sin valor
+
+const producto = true; //reasignando el valor de la variable
+
+const pro1 = "computadora",
+  pro2 = "tablet",
+  pro3 = "celular";
+
+//estilos para las variables
+const nombre_producto = "monitor HD"; //underscore
+const nombreProducto = "monitor HD"; //camelcase
+const NombreProducto = "monitor HD"; //Pascalcase
+const nombreproducto = "monitor HD"; //lowercase
+console.log(producto);
+/*var nombre = "Vetdy";*/
