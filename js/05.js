53d8910e36d443687aef2d297e4d1ba9494b52f3
vetdy03 <cocaever0@gmail.com>
2025-12-05 00:42:53 -0400

    javascript - 108. Números y Operadores

---
+++ js/05.js
@@ -0,0 +1,15 @@
+//number o numeros
+const nombre = "Vetdy";
+const numero1 = 10;
+const numero2 = 19;
+const numero3 = .5;
+
+console.log(numero1 + numero2);
+console.log(numero2);
+console.log(numero3);
+console.log(numero1 * numero2);
+console.log(numero2 / numero1);
+console.log(numero1 - numero2);
+console.log(numero1 / numero3);
+console.log(numero2 % numero1);
+console.log("el nombre es:"+ nombre);
