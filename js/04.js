882bce94877ee2e09c7ce0030303f814a6fcfadf
vetdy03 <cocaever0@gmail.com>
2025-12-05 00:28:25 -0400

    javascript - 106. Strings o Cadenas de Texto

---
+++ js/04.js
@@ -0,0 +1,9 @@
+//string o cadenas de texto
+const producto = "monitor e 20 pulgada\"";
+const producto2 = String("monitor e 50 pulgadas");
+const producto3 = new String("monitor e 60 pulgadas");
+
+console.log(producto);
+console.log(producto2);
+console.log(typeof producto3);
+
