1321c224410f6c855e517093e125f15176a45dfe
vetdy03 <cocaever0@gmail.com>
2025-12-05 00:09:00 -0400

    javascript - variables en javascript

---
+++ js/01.js
@@ -0,0 +1,17 @@
+//variables
+var producto = "audifonos gamer"; //iniciar variable y asignar valor
+var disponible; //iniciar variable sin valor
+
+// producto = true; //reasignando el valor de la variable
+
+var pro1 = "computadora",
+  pro2 = "tablet",
+  pro3 = "celular";
+
+//estilos para las variables
+var nombre_producto = "monitor HD"; //underscore
+var nombreProducto = "monitor HD"; //camelcase
+var NombreProducto = "monitor HD"; //Pascalcase
+var nombreproducto = "monitor HD"; //lowercase
+console.log(producto);
+/*var nombre = "Vetdy";*/
