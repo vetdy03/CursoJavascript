6e96664bd6eaffe0719b282da037f3917f87a5c0
vetdy03 <cocaever0@gmail.com>
2025-12-06 23:58:14 -0400

    javascript - 109. El Objeto Math en JS parte 2

--- js/06.js
+++ js/06.js
@@ -1,5 +1,25 @@
 let res;
 res= Math.PI;
-let res2 = Math.round(3.7); // redondea al entero mas cercano
+let res2 = Math.round(3.9); // redondea al entero mas cercano
+let res3 = Math.ceil(4.2); // redondea hacia arriba
+let res4 = Math.floor(4.7); // redondea hacia abajo
+let res5 = Math.sqrt(16); // raiz cuadrada
+let res6 = Math.abs(-5); // valor absoluto  
+let res7 = Math.pow(2,3); // potencia
+let res8 = Math.min(3,5,1,7,2); // minimo
+let res9 = Math.max(3,5,1,7,2); // maximo
+let res10 = Math.random(); // numero aleatorio entre 0 y 1  
+
 console.log(res);
 console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);
+console.log(res7);
+console.log(res8);
+console.log(res9);
+console.log(res10); 
+
+
+
