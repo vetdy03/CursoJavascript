bfe35fdf79e15b13cfa3f26679f37394d000987c
vetdy03 <cocaever0@gmail.com>
2025-12-09 23:58:51 -0400

    javascript - 113 OBJETCS

---
+++ js/10.js
@@ -0,0 +1,12 @@
+const nombrePRECIO = "Monitor de 20 Pulgadas";
+const precioPRECIO = 300;
+const disponiblePRECIO = true;
+
+const producto = {
+    nombreProducto: "Monitor de 20 Pulgadas", 
+    precioProducto: 300,
+    disponibleProducto: true
+}
+console.log(producto);
+console.log(producto.nombreProducto);
+console.log(producto.precioProducto);
