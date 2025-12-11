ce32e9e49164226a2f7f439726916d0f95e379a4
vetdy03 <cocaever0@gmail.com>
2025-12-10 23:53:38 -0400

    javascript - 114. Modificar Objetos

--- js/10.js
+++ js/10.js
@@ -1,12 +1,19 @@
-const nombrePRECIO = "Monitor de 20 Pulgadas";
-const precioPRECIO = 300;
-const disponiblePRECIO = true;
+// const nombrePRECIO = "Monitor de 20 Pulgadas";
+// const precioPRECIO = 300;
+// const disponiblePRECIO = true;
 
 const producto = {
     nombreProducto: "Monitor de 20 Pulgadas", 
     precioProducto: 300,
-    disponibleProducto: true
+    disponibleProducto: true,
 }
+
+//agregar nuevas propiedades
+producto.imagen = "imagen.jpg";
 console.log(producto);
-console.log(producto.nombreProducto);
-console.log(producto.precioProducto);
+
+//eliminar propiedades
+delete producto.precioProducto;
+console.log(producto);
+// console.log(producto.nombreProducto);
+// console.log(producto.precioProducto);
