e13f4f0a3f66f24d513c264750cf6b2e99b0034d
vetdy03 <cocaever0@gmail.com>
2025-12-11 10:51:26 -0400

    javascript - 116 metodos de los objetos

--- js/12.js
+++ js/12.js
@@ -6,11 +6,11 @@ const producto = {
     disponible: true,
 } 
 Object.freeze(producto);
-
-// producto.precio = 600; //no se puede modificar
-// delete producto.disponible; //no se puede eliminar
+producto.precio = 600; //no se puede modificar
+delete producto.disponible; //no se puede eliminar
 producto.imagen = "imagen.jpg";
-
 console.log(Object.isFrozen(producto)); //true
-
 console.log(producto);
+
+//seal
+Object.seal(producto); //no permite eliminar o agregar nuevas propiedades pero si modificar las existentes
