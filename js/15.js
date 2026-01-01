911009f31b8b853204445458ba952e3f4c5f7512
vetdy03 <cocaever0@gmail.com>
2026-01-01 18:26:35 -0400

    javascript - 120. mas metodos de arrays

--- js/15.js
+++ js/15.js
@@ -1,22 +1,56 @@
-//arreglso o arrays
-
-const nums = [ 10, 20, 30, 40, 50 ];
-
+//array methods
 
 const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo');
 console.table(meses);
 
-// nums[5] = 60; //agrega un nuevo elemento al arreglo
-//nums.push(70); //agrega un nuevo elemento al final del arreglo
+const carrito = [
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Television 50 pulgadas', precio: 700},
+    {nombre: 'Tablet', precio: 300},
+    {nombre: 'Audifonos', precio: 200},
+    {nombre: 'Teclado', precio: 50},
+    {nombre: 'Celular', precio: 500},
+    {nombre: 'Bocinas', precio: 300},
+    {nombre: 'Laptop', precio: 800},
+    {nombre: 'Reloj inteligente', precio: 200},
+    
+];
 
-nums.unshift(0); //agrega al inicio del arreglo
+//foreach
+meses.forEach( function(mes) {
+    if(mes == 'Marzo'){
+        console.log('marzo si existe');
+    }
+});
 
-//algunos metodos para eliminar elementos de un arreglo
-nums.pop(); //elimina el ultimo elemento del arreglo
-nums.shift(); //elimina el primer elemento del arreglo
+//includes
+const resultado = meses.includes('Diciembre'); // esto es para arreglos simples, hace que busque un valor en el arreglo
+const resultado2 = meses.includes('Marzo');
+console.log(resultado);
+console.log(resultado2);
 
-//splice para eliminar o agregar elementos en cualquier posicion
-// nums.splice(2, 1); //elimina el elemento en la posicion 2
-// nums.splice(2, 0, 25); //agrega el elemento 25 en la posicion 2
-// nums.splice(4, 1, 35); //reemplaza el elemento en la posicion 4 por 35
-console.table(nums);
+//some ideal para arreglos de objetos
+const existe = carrito.some( function( producto ) {
+    return producto.nombre === 'Celular';
+    
+} );
+console.log(existe);
+
+//reduce
+const total = carrito.reduce( function( total, producto ){ return total + producto.precio; }, 0 ); //el 0 es el valor inicial
+console.log(total);
+
+//filter
+const resultado3 = carrito.filter( function( producto ){
+    return producto.precio < 400;
+})
+console.table(resultado3);
+const resultado4 = carrito.filter( function( producto ){
+    return producto.precio !== 200;
+})
+console.table(resultado4);
+
+const resultado5 = carrito.filter( function( producto ){
+    return producto.nombre == 'Celular';
+})
+console.table(resultado5);
