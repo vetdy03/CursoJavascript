0d16ce81b7b09db25c7e43227184684d65326451
vetdy03 <cocaever0@gmail.com>
2026-01-11 17:14:48 -0400

    javascript - 130 - for loops

---
+++ js/24.js
@@ -0,0 +1,37 @@
+//for loop 
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i ++){
+//     if(i % 2 === 0){
+//         console.log(`El num ${i} es par `);
+//     }else{
+//         console.log(`El num ${i} es impar `);
+//     }
+// }
+
+const carrito = [
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Television 50 pulgadas', precio: 700},
+    {nombre: 'Tablet', precio: 300},
+    {nombre: 'Audifonos', precio: 200},
+    {nombre: 'Teclado', precio: 50},
+    {nombre: 'Celular', precio: 500},
+    {nombre: 'Bocinas', precio: 300},
+    {nombre: 'Laptop', precio: 800},
+    {nombre: 'Reloj inteligente', precio: 200},
+    
+];
+for (let i = 0; i < carrito.length; i++){
+    console.log(carrito[i].nombre);
+}
+
+
+
+
+
+
+//while loop
+
+//do while loop
