15f3f6dbcfe37862d3896e9638911b9289e0b141
vetdy03 <cocaever0@gmail.com>
2026-01-11 17:04:37 -0400

    javascript - 128 - 129 - switch

---
+++ js/23.js
@@ -0,0 +1,16 @@
+const metodoPago = 'tarjeta';
+
+switch (metodoPago){
+    case 'tarjeta':
+        console.log('pagaste con tarjeta');
+        break;
+    case 'bitcoin':
+        console.log('pagaste con bitcoin');
+        break;
+    case 'efectivo':
+        console.log('pagaste con efectivo');
+        break;
+    default:
+        console.log('aun no has pagado');
+        break;
+}
