9f100b342328ec55c89f44ad8d54741f7fe7706f
vetdy03 <cocaever0@gmail.com>
2026-01-15 23:57:25 -0400

    javascript - version mejorada

--- js/27.js
+++ js/27.js
@@ -1,1 +1,17 @@
 //POO
+const metodoPago = 'tarjeta';
+
+switch (metodoPago){
+    case 'tarjeta':
+        console.log('pagaste con tarjeta');
+        break;
+    case 'bitcoin':
+        console.log('pagaste con bitcoin');
+        break;
+    case 'efectivo':
+        console.log('pagaste con efectivo');
+        break;
+    default:
+        console.log('aun no has pagado');
+        break;
+}
---
+++ js/output1.py
@@ -0,0 +1,3 @@
+x = [1, 2, 7]
+print(x[-1])  # Output: 7
+print(x[-2])  # Output: 2
