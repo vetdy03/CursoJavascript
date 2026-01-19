e806bdeb7c65488196b52c1baf0c0997b9bc324a
vetdy03 <cocaever0@gmail.com>
2026-01-19 15:10:58 -0400

    137 herencia en poo

--- js/28.js
+++ js/28.js
@@ -21,5 +21,19 @@ class Producto {
 const producto2 = new Producto('monitor curvo de 49"', 500);
 const producto3 = new Producto('laptop hp', 7000);
 
+class Libro extends Producto {
+    constructor(nombre, precio, isbn){
+        super(nombre, precio);
+        this.isbn = isbn;
+
+    }
+    formatearProducto(){
+        return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+    }
+}
+const libro = new Libro('Javascript', 150, 'asd3232322');
+
+
+console.log(libro.formatearProducto());
 console.log(producto2);
 console.log(producto3);
