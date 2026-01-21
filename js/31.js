4a597e8b6fc40104c87d27a067a78f337e6e8fd1
vetdy03 <cocaever0@gmail.com>
2026-01-21 19:09:48 -0400

    140 notificaciones API

---
+++ js/31.js
@@ -0,0 +1,13 @@
+const boton = document.querySelector('#botonParaVerNotifcacion') 
+boton.addEventListener('click', () => {
+    Notification.requestPermission()
+    .then( resultado => console.log(`El resultado es ${resultado}`) )
+    // console.log('clickando');
+})
+
+if(Notification.permission === 'granted'){
+    new Notification('Esta es la notificacion', {
+        icon: 'img/ccj.png',
+        body: 'Esto fue echo por vetdy03'
+    })
+}
