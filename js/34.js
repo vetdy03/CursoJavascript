8efa183d0ea0d4fc471f5d340836ca3eed014876
vetdy03 <cocaever0@gmail.com>
2026-01-24 10:51:20 -0400

    143 - javascript fetch API con async await

--- js/34.js
+++ js/34.js
@@ -1,6 +1,8 @@
-function obtenerEmpleados() {
-    const archivo = 'empleados.json';
-    const 
-
+async function obtenerEmpleados() {
+    const archivo = 'empledados.json'; //guardamos en un variable la ruta del archivo
+    const resultado = await fetch(archivo); //fetch devuelve una promesa y await espera a que se resuelva y hace que el codigo no continue hasta que se resuelva
+    console.log("no llega aqui hasta que fetch se resuelva");
+    const datos = await resultado.json(); //convertimos la respuesta a json tipo casting
+    console.log(datos);
 }
 obtenerEmpleados();  
