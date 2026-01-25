fa0971ef4f5c522210475d75f78664572ed4f983
vetdy03 <cocaever0@gmail.com>
2026-01-25 10:58:00 -0400

    146. - javascript crear HTML con createElement en js

--- docsGuia/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ docsGuia/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,24 +1,39 @@
 //queryselector
 const heading = document.querySelector('.header__texto h2'); // 0 or 1 elemento
-console.log(heading);
-heading.textContent = 'vetdy03 is the king';
-heading.classList.add('Nueva_clase');
+// console.log(heading);
+// heading.textContent = 'vetdy03 is the king';
+// heading.classList.add('Nueva_clase');
 
 //query selectorall
 
 const enlaces = document.querySelectorAll('.navegacion a'); // todos los elementos que coincidan
 //console.log(enlaces);
-enlaces[0].textContent = 'fue un nosotros';
+// enlaces[0].textContent = 'fue un nosotros';
 // enlaces[0].remove();
-enlaces[0].href = 'https://www.google.com';
-enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].href = 'https://www.google.com';
+// enlaces[0].classList.remove('navegacion__enlace');
 
 
 //getelementbyid casi ya no se usa
 const heading2 = document.getElementById('heading2');
-console.log('heading2');
+// console.log('heading2');
 
+//generar un nuevo enlace
+const nuevoEnlace = document.createElement('A');
+// console.log(nuevoEnlace);
 
+//add el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+//add el text
+nuevoEnlace.textContent = 'Nuevo enlace';
+//agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+console.log(nuevoEnlace);
+
+//add to document
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
 
 //getelementbyclassname
 
