3a524de10a953662a357473ab726434b5e83f0b9
vetdy03 <cocaever0@gmail.com>
2026-01-25 08:45:29 -0400

    144. Seleccionar Elementos con querySelector

---
+++ docsGuia/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,17 @@
+//queryselector
+const heading = document.querySelector('.header__texto h2');
+console.log(heading);
+heading.textContent = 'vetdy03 is the king';
+heading.classList.add('Nueva-clase');
+
+//query selectorall
+
+
+//getelementbyid
+
+//getelementbyclassname
+
+
+//selecionar una clase con .
+//por id con una numeral #
+// si te equivocas en el nombre del selecto js te dira null en la consola
