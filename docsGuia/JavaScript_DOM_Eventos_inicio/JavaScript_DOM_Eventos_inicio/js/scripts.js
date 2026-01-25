41845f815e7cad3fa6e1a4a43d2b442889664d69
vetdy03 <cocaever0@gmail.com>
2026-01-25 10:09:26 -0400

    145. - javascript seleccionar elementos con queryselectorAll

--- docsGuia/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ docsGuia/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,17 +1,28 @@
 //queryselector
-const heading = document.querySelector('.header__texto h2');
+const heading = document.querySelector('.header__texto h2'); // 0 or 1 elemento
 console.log(heading);
 heading.textContent = 'vetdy03 is the king';
-heading.classList.add('Nueva-clase');
+heading.classList.add('Nueva_clase');
 
 //query selectorall
 
+const enlaces = document.querySelectorAll('.navegacion a'); // todos los elementos que coincidan
+//console.log(enlaces);
+enlaces[0].textContent = 'fue un nosotros';
+// enlaces[0].remove();
+enlaces[0].href = 'https://www.google.com';
+enlaces[0].classList.remove('navegacion__enlace');
 
-//getelementbyid
+
+//getelementbyid casi ya no se usa
+const heading2 = document.getElementById('heading2');
+console.log('heading2');
+
+
 
 //getelementbyclassname
 
 
 //selecionar una clase con .
 //por id con una numeral #
-// si te equivocas en el nombre del selecto js te dira null en la consola
+// si te equivocas en el nombre del selecto js te dira null en la consola 
